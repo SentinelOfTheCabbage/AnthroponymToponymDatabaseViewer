f4e6ffd415fd4dabcad45a8ea7c3c77c5cf065f8
Наиль Сулейманов <nmsuleymanov@miem.hse.ru>
2022-11-05 13:03:42 +0300

    feat: references 'json' data type changed to 'string'

--- src/db/anthroponym_reference.py
+++ src/db/anthroponym_reference.py
@@ -1,5 +1,5 @@
 from flask_admin.contrib.sqla import ModelView
-from sqlalchemy.types import BIGINT, JSON
+from sqlalchemy.types import BIGINT, VARCHAR
 from .connection import db
 
 
@@ -7,8 +7,7 @@ class AnthroponymReference(db.Model):
     __tablename__ = 'anthroponym_reference'
     literature_id = db.Column(BIGINT, db.ForeignKey('literature.literature_id'), nullable=False, primary_key=False)
     anthroponym_id = db.Column(BIGINT, db.ForeignKey('anthroponym.anthroponym_id'), nullable=False, primary_key=False)
-    # TODO : make pages String, not JSON. Like '1, 15-22'
-    pages = db.Column(JSON, nullable=False)
+    pages = db.Column(VARCHAR(100), nullable=False)
 
     __mapper_args__ = {
         "primary_key": [literature_id, anthroponym_id]
--- src/db/toponym_reference.py
+++ src/db/toponym_reference.py
@@ -1,4 +1,4 @@
-from sqlalchemy.types import BIGINT, JSON
+from sqlalchemy.types import BIGINT, VARCHAR
 from .connection import db
 
 
@@ -6,7 +6,7 @@ class ToponymReference(db.Model):
     __tablename__ = 'toponym_reference'
     literature_id = db.Column(BIGINT, db.ForeignKey('literature.literature_id'), nullable=False)
     toponym_id = db.Column(BIGINT, db.ForeignKey('toponym.toponym_id'), nullable=False)
-    pages = db.Column(JSON, nullable=False)
+    pages = db.Column(VARCHAR(100), nullable=False)
 
     __mapper_args__ = {
         "primary_key": [literature_id, toponym_id]
