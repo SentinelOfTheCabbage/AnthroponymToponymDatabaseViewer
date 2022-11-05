b0dd101d122426b70614e20e36eb44bee591b790
Наиль Сулейманов <nmsuleymanov@miem.hse.ru>
2022-11-05 12:41:34 +0300

    style: model views extracted from src module to db submodule

--- src/db/__init__.py
+++ src/db/__init__.py
@@ -1,3 +1,6 @@
+from flask_admin.contrib.sqla import ModelView
+from src.configs.constants import AnthTables, Cat, ToponTables
+
 from .anthroponym import Anthroponym
 from .anthroponym_image import AnthroponymImage, AnthroponymImageModelView
 from .anthroponym_reference import AnthroponymReference, AnthroponymReferenceModelView
@@ -6,3 +9,13 @@ from .toponym import Toponym
 from .toponym_image import ToponymImage, ToponymImageModelView
 from .toponym_reference import ToponymReference
 from .connection import db
+
+AnthroponymView = ModelView(Anthroponym, db.session, category=Cat.ANTHROPONYM, name=AnthTables.ANTHROPONYM)
+AnthroponymImageView = AnthroponymImageModelView(AnthroponymImage, db.session, category=Cat.ANTHROPONYM, name=AnthTables.IMAGE)
+AnthroponymReferenceView = AnthroponymReferenceModelView(AnthroponymReference, db.session, category=Cat.ANTHROPONYM, name=AnthTables.REFERENCE)
+
+ToponymView = ModelView(Toponym, db.session, category=Cat.TOPONYM, name=ToponTables.TOPONYM)
+ToponymImageView = ToponymImageModelView(ToponymImage, db.session, category=Cat.TOPONYM, name=ToponTables.IMAGE)
+ToponymReferenceView = ModelView(ToponymReference, db.session, category=Cat.TOPONYM, name=ToponTables.REFERENCE)
+
+LiteratureView = ModelView(Literature, db.session, category=Cat.OTHER, name='Литература')
--- src/db/anthroponym_image.py
+++ src/db/anthroponym_image.py
@@ -1,12 +1,11 @@
 from flask_admin.contrib.sqla import ModelView
-from sqlalchemy.schema import Column, ForeignKey
 from sqlalchemy.types import BIGINT, BLOB
 from .connection import db
 
 
 class AnthroponymImage(db.Model):
     __tablename__ = 'anthroponym_image'
-    anthroponym_id = db.Column(BIGINT, ForeignKey('anthroponym.anthroponym_id'))
+    anthroponym_id = db.Column(BIGINT, db.ForeignKey('anthroponym.anthroponym_id'))
     img = db.Column(BLOB)
 
     __mapper_args__ = {
@@ -14,6 +13,6 @@ class AnthroponymImage(db.Model):
     }
 
 
-class AnthroponymImageModelView(ModelView):
+class AnthroponymImageView(ModelView):
     # TODO: make file uploading page!
     create_template = 'create_anthroponym_image.html'
