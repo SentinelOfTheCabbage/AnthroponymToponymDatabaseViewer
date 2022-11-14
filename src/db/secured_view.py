f67491d8e8fb66b6dfaae3bf6e4e8415b233ed80
Наиль Сулейманов <nmsuleymanov@miem.hse.ru>
2022-11-14 11:52:50 +0300

    feat: login, logout buttons and edit,create,delet right added

--- src/db/secured_view.py
+++ src/db/secured_view.py
@@ -5,12 +5,24 @@ from flask_security import current_user
 
 class SecuredMixin():
     def is_accessible(self):
-        return (current_user.is_active and current_user.is_authenticated)
+        # return (current_user.is_active and current_user.is_authenticated)
+        return True
 
     def _handle_view(self, name, **kwargs):
         if not self.is_accessible():
             return redirect(url_for('security.login'))
 
+    @property
+    def can_create(self):
+        return bool(set(['moderator', 'admin']) & set([role.name for role in current_user.roles]))
+
+    @property
+    def can_edit(self):
+        return self.can_create
+
+    @property
+    def can_delete(self):
+        return self.can_create
 
 class SecuredModelView(SecuredMixin, ModelView):
     pass
