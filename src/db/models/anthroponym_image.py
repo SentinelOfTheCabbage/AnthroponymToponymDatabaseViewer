2cafc8a96e18b482bee078ca85729564d0a6ec47
Наиль Сулейманов <nmsuleymanov@miem.hse.ru>
2022-11-14 11:56:41 +0300

    feat: custom forms for image tables added

--- src/db/models/anthroponym_image.py
+++ src/db/models/anthroponym_image.py
@@ -1,4 +1,8 @@
+import os
+import random
+
 from sqlalchemy.types import BIGINT, BLOB
+from flask_admin import form
 
 from ..secured_view import SecuredModelView
 from ..connection import db
@@ -15,5 +19,48 @@ class AnthroponymImage(db.Model):
 
 
 class AnthroponymImageModelView(SecuredModelView):
-    # TODO: make file uploading page!
-    create_template = 'create_anthroponym_image.html'
+    column_list = ['Anthroponym', 'Image file']
+
+    def _list_thumbnail(view, context, model, name):
+        # TODO: how to display it on view page?
+        return "TODO"
+
+
+    column_formatters = {
+        'img': _list_thumbnail
+    }
+
+    form_extra_fields = {
+        'img_file': form.FileUploadField('Image file', allowed_extensions=['jpg','png','jpeg', 'svg', 'gif'])
+    }
+
+    form_excluded_columns = ['img']
+    def _change_img_data(self, _form):
+        try:
+            storage_file = _form.img_file.data
+
+            if storage_file is not None:
+                hash = random.getrandbits(128)
+                ext = storage_file.filename.split('.')[-1]
+                path = '%s.%s' % (hash, ext)
+                # TODO: change img field type to VARCHAR ?
+                storage_file.save(
+                    os.path.join(os.environ['IMG_STORAGE'], path)
+                )
+
+                del _form.file
+
+        except Exception as ex:
+            pass
+
+        return _form
+
+    def edit_form(self, obj=None):
+        return self._change_img_data(
+            super(AnthroponymImageModelView, self).edit_form(obj)
+        )
+
+    def create_form(self, obj=None):
+        return self._change_img_data(
+            super(AnthroponymImageModelView, self).create_form(obj)
+        )
--- src/db/models/toponym_image.py
+++ src/db/models/toponym_image.py
@@ -1,4 +1,7 @@
 from sqlalchemy.types import BIGINT, BLOB
+from flask_admin import form
+import random
+import os
 
 from ..secured_view import SecuredModelView
 from ..connection import db
@@ -14,5 +17,48 @@ class ToponymImage(db.Model):
     }
 
 class ToponymImageModelView(SecuredModelView):
-    edit_template = 'edit_toponym_image.html'
-    create_template = 'create_toponym_image.html'
+    column_list = ['Troponym', 'Image file']
+
+    def _list_thumbnail(view, context, model, name):
+        # TODO: how to display it on view page?
+        return "TODO"
+
+
+    column_formatters = {
+        'img': _list_thumbnail
+    }
+
+    form_extra_fields = {
+        'img_file': form.FileUploadField('Image file', allowed_extensions=['jpg','png','jpeg', 'svg', 'gif'])
+    }
+
+    form_excluded_columns = ['img']
+    def _change_img_data(self, _form):
+        try:
+            storage_file = _form.img_file.data
+
+            if storage_file is not None:
+                hash = random.getrandbits(128)
+                ext = storage_file.filename.split('.')[-1]
+                path = '%s.%s' % (hash, ext)
+                # TODO: change img field type to VARCHAR ?
+                storage_file.save(
+                    os.path.join(os.environ['IMG_STORAGE'], path)
+                )
+
+                del _form.file
+
+        except Exception as ex:
+            pass
+
+        return _form
+
+    def edit_form(self, obj=None):
+        return self._change_img_data(
+            super(ToponymImageModelView, self).edit_form(obj)
+        )
+
+    def create_form(self, obj=None):
+        return self._change_img_data(
+            super(ToponymImageModelView, self).create_form(obj)
+        )
